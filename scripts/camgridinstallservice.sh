072776b1f889d5ee4295e8015abc8124b37bda70
Chris Paiano <chris.paiano@gmail.com>
2021-05-29 17:17:20 -0700

    Fixed install folders

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -2,12 +2,12 @@
 
 echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 camgridservicestop.sh
-cp -vf "$HOME/reference/xsessionrc" "$HOME/.xsessionrc"
+cp -vf "$HOME/picamframegrid/reference/xsessionrc" "$HOME/.xsessionrc"
 chmod +x "$HOME/.xsessionrc"
-sudo cp -vf "$HOME/reference/xsession.target" "/etc/systemd/user/"
-sudo cp -vf "$HOME/reference/camgrid.service" "/etc/systemd/user/"
+sudo cp -vf "$HOME/picamframegrid/reference/xsession.target" "/etc/systemd/user/"
+sudo cp -vf "$HOME/picamframegrid/reference/camgrid.service" "/etc/systemd/user/"
 mkdir -vp "$HOME/.camgrid"
-cp --verbose --no-clobber "$HOME/reference/camgrid.conf" "$HOME/.camgrid/"
+cp --verbose --no-clobber "$HOME/picamframegrid/reference/camgrid.conf" "$HOME/.camgrid/"
 systemctl --user daemon-reload
 systemctl --user enable camgrid
 camgridservicestart.sh
