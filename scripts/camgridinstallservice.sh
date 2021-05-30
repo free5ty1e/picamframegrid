2e768e555fef8ae0b83ed3dce305d18635ad2910
Chris Paiano <chris.paiano@gmail.com>
2021-05-29 17:11:34 -0700

    fixed install script

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -3,12 +3,12 @@
 echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 # sudo service camgrid stop
 systemctl --user stop camgrid
-cp -vf debianusbfileserver/reference/xsessionrc "$HOME/.xsessionrc"
+cp -vf $HOME/reference/xsessionrc "$HOME/.xsessionrc"
 chmod +x /home/pi/.xsessionrc
-sudo cp -vf debianusbfileserver/reference/xsession.target /etc/systemd/user/
-sudo cp -vf debianusbfileserver/reference/camgrid.service /etc/systemd/user/
+sudo cp -vf $HOME/reference/xsession.target /etc/systemd/user/
+sudo cp -vf $HOME/reference/camgrid.service /etc/systemd/user/
 mkdir -vp "$HOME/.camgrid"
-cp --verbose --no-clobber debianusbfileserver/reference/camgrid.conf "$HOME/.camgrid/"
+cp --verbose --no-clobber $HOME/reference/camgrid.conf "$HOME/.camgrid/"
 systemctl --user daemon-reload
 systemctl --user enable camgrid
 camgridservicestart.sh
