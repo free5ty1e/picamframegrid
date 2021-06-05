49db2d8be441397e66ef6f499b72659a95ac9945
Chris Paiano <chris.paiano@gmail.com>
2021-06-05 14:18:57 -0700

    Further separated camgrid install for desktop version out from default

--- scripts/camgridinstallaptpackages.sh
+++ scripts/camgridinstallaptpackages.sh
@@ -7,3 +7,12 @@ echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sour
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
 sudo apt update
 sudo apt install -y log2ram
+
+echo "Installing raspi2bmp for framebuffer screenshots..."
+pushd ~
+git clone https://github.com/ThinhPhan/raspi2bmp.git
+pushd raspi2bmp
+make
+sudo cp raspi2bmp /usr/local/bin/
+popd
+popd
--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -2,9 +2,6 @@
 
 echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 camgridservicestop.sh
-cp -vf "$HOME/picamframegrid/reference/xsessionrc" "$HOME/.xsessionrc"
-chmod +x "$HOME/.xsessionrc"
-sudo cp -vf "$HOME/picamframegrid/reference/xsession.target" "/etc/systemd/user/"
 sudo cp -vf "$HOME/picamframegrid/reference/camgrid.service" "/etc/systemd/user/"
 mkdir -vp "$HOME/.camgrid"
 cp --verbose --no-clobber "$HOME/picamframegrid/reference/camgrid.conf" "$HOME/.camgrid/"
