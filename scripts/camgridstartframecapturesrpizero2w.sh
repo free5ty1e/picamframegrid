1bd7741b61de44d0def7a6cf2bb933ffc2748ad4
Chris Paiano <chris.paiano@gmail.com>
2025-01-24 12:27:47 -0500

    separate camgrid loop cmds for rpi zero 2 w

---
+++ scripts/camgridstartframecapturesrpizero2w.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+. $HOME/.camgrid/camgrid.conf
+
+FFMPEG_INPUT_PARAMETERS=""
+FFMPEG_FILTER_PARAMETERS_A=""
+FFMPEG_FILTER_PARAMETERS_B=""
+
+for i in "${!RTSP_STREAM_URLS[@]}"; do 
+	STREAM_URL=${RTSP_STREAM_URLS[$i]}
+	STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
+	STREAM_XOFFSET=${RTSP_STREAM_XOFFSETS[$i]}
+	STREAM_YOFFSET=${RTSP_STREAM_YOFFSETS[$i]}
+	STREAM_FPS=${RTSP_STREAM_FPS[$i]}
+
+
+	echo "======----->>>Starting RTSP stream (# $i named $STREAM_TITLE) capture of URL $STREAM_URL at $CAPTURE_RESOLUTION $STREAM_FPS FPS to $CAPTURE_LOCATION/$STREAM_TITLE.$CAPTURE_FORMAT or to location $STREAM_XOFFSET, $STREAM_YOFFSET..."
+	
+# 	XVAL=`echo "$i*480" | bc`
+
+# 	FFMPEG_INPUT_PARAMETERS="$FFMPEG_INPUT_PARAMETERS-i $STREAM_URL "
+# 	FFMPEG_FILTER_PARAMETERS_A="$FFMPEG_FILTER_PARAMETERS_A
+# [$i:v] setpts=PTS-STARTPTS, scale=480x270 [video$i];"
+
+# 	if [ $i > 0 ]; then
+#  		FFMPEG_FILTER_PARAMETERS_B="$FFMPEG_FILTER_PARAMETERS_B [tmp$i];
+# [tmp$i]"
+#  	else
+#  		FFMPEG_FILTER_PARAMETERS_B="$FFMPEG_FILTER_PARAMETERS_B[base]"
+# 	fi
+
+# 	FFMPEG_FILTER_PARAMETERS_B="$FFMPEG_FILTER_PARAMETERS_B[video$i] overlay=shortest=0:x=$XVAL"
+
+	camgridstartframecapturerpizero2w.sh "$STREAM_URL" "$STREAM_TITLE" "$STREAM_XOFFSET" "$STREAM_YOFFSET" "$STREAM_FPS" &
+
+	# sudo ffmpeg -loglevel fatal -i "$STREAM_URL" -vf fps=fps=$STREAM_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION "$CAPTURE_LOCATION/$STREAM_TITLE.jpg" </dev/null &
+	#-stimeout $RTSP_TIMEOUT -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -f fifo -fifo_format flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recover_any_error 1
+	export CAPTURE_STREAM_PID=$!
+	echo "Waiting a second to stagger the streams..."
+	sleep 1
+done
+
+
+# ffmpeg -loglevel fatal -threads 1 -stimeout $RTSP_TIMEOUT -analyzeduration 10M -probesize 10M -re $FFMPEG_INPUT_PARAMETERS -filter_complex "\"
+# nullsrc=size=1280x1024 [base];
+# $FFMPEG_FILTER_PARAMETERS_A
+# $FFMPEG_FILTER_PARAMETERS_B
+# \"" -an -pix_fmt bgra -f fbdev /dev/fb0
+
+# sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_2" -vf fps=fps=$STREAM_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream2.jpg </dev/null &
+# export CAPTURE_STREAM_PID_2=$!
