db4e6d8195c005b12a0c6a09cffa021add9785bf
Chris Paiano <chris.paiano@gmail.com>
2025-01-23 23:20:28 -0500

    Split off omxplayer so that an install could be attempted on a pi zero w 2

--- scripts/camgridinstallaptpackages.sh
+++ scripts/camgridinstallaptpackages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y --no-install-recommends xserver-xorg-core xserver-xorg xfonts-base xinit xfce4 desktop-base lightdm xloadimage fbi fim imagemagick ffmpeg vlc omxplayer xfce4-goodies inotify-tools scrot
+sudo apt install -y --no-install-recommends xserver-xorg-core xserver-xorg xfonts-base xinit xfce4 desktop-base lightdm xloadimage fbi fim imagemagick ffmpeg vlc xfce4-goodies inotify-tools scrot
+sudo apt install -y --no-install-recommends omxplayer
 
 echo "Installing log2ram from https://github.com/azlux/log2ram - see instructions there if this fails..."
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
