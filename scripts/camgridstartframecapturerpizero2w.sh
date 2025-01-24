40807d18a4808611b8d5b9e22c8f83eb7e7d33e9
Chris Paiano <chris.paiano@gmail.com>
2025-01-24 11:04:03 -0500

    Experimental pi zero frame cap script

---
+++ scripts/camgridstartframecapturerpizero2w.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. $HOME/.camgrid/camgrid.conf
+
+echo "start frame capture"
+STREAM_URL="$1"
+STREAM_TITLE="$2"
+XOFFSET="$3"
+YOFFSET="$4"
+STREAM_FPS="$5"
+echo "parameter 1 should be STREAM_URL and it is $STREAM_URL"
+echo "parameter 2 should be STREAM_TITLE and it is $STREAM_TITLE"
+echo "parameter 3 should be XOFFSET and it is $XOFFSET"
+echo "parameter 4 should be YOFFSET and it is $YOFFSET"
+echo "parameter 5 should be STREAM_FPS and it is $STREAM_FPS"
+echo "======----->>>Starting RTSP stream (# $i named $STREAM_TITLE) capture of URL $STREAM_URL at $CAPTURE_RESOLUTION $STREAM_FPS FPS to $CAPTURE_LOCATION/$STREAM_TITLE.$CAPTURE_FORMAT..."
+
+while true; do 
+	if [ "$CAMGRID_METHOD" == "desktop_xfce" ]; then
+		nice -10 ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -skip_frame nokey -i "$STREAM_URL" -vf fps=fps=$STREAM_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION "$CAPTURE_LOCATION/$STREAM_TITLE.$CAPTURE_FORMAT" </dev/null; 
+	elif [ "$CAMGRID_METHOD" == "direct_to_framebuffer" ]; then
+		# nice -10 ffmpeg -loglevel fatal -threads 1 -stimeout $RTSP_TIMEOUT -lowres 0 -skip_loop_filter 1 -skip_frame nokey -ticks_per_frame 2 -flags2 fast -ec favor_inter -idct simpleauto -err_detect ignore_err -strict unofficial -bug autodetect -i "$STREAM_URL" -an -y -filter_complex "mpdecimate, setpts=N/$STREAM_FPS/TB, fps=fps=$STREAM_FPS, scale=$CAPTURE_RESOLUTION" -pix_fmt bgra -update 1 -f fbdev -xoffset $XOFFSET -yoffset $YOFFSET /dev/fb0 </dev/null;
+		nice -10 ffmpeg -threads 1 -stimeout $RTSP_TIMEOUT -lowres 0 -skip_loop_filter 1 -skip_frame nokey -ticks_per_frame 2 -flags2 fast -ec favor_inter -idct simpleauto -err_detect ignore_err -strict unofficial -bug autodetect -i "$STREAM_URL" -an -y -filter_complex "mpdecimate, setpts=N/$STREAM_FPS/TB, fps=fps=$STREAM_FPS, scale=$CAPTURE_RESOLUTION" -pix_fmt bgra -update 1 -f fbdev -xoffset $XOFFSET -yoffset $YOFFSET /dev/fb0 </dev/null;
+	fi
+done
+
+#Auto-restart loop:
+# until $cmd ; do
+#         echo "restarting ffmpeg command for $STREAM_TITLE..."
+#         sleep 2
+# done
+
+# sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_2" -vf fps=fps=$STREAM_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream2.jpg </dev/null &
+# export CAPTURE_STREAM_PID_2=$!
