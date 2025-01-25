fe827052a2952b383301f97a6fbe6f81dfaff364
Chris Paiano <chris.paiano@gmail.com>
2025-01-24 23:44:06 -0500

    Pointed main camgridloop to new pi zero 2 w scripts

--- scripts/camgridloop.sh
+++ scripts/camgridloop.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-. $HOME/.camgrid/camgrid.conf
 
-echo "User is `id -nu`, should be pi for this to work..."
-# if [[ `id -nu` != "pi" ]];then
-#    echo "Not pi user, exiting.."
-#    exit 1
-# fi
-
-camgridstopframecaptures.sh
-camgridstartframecaptures.sh
-sleep 10
-
-camgrideventloop.sh
+camgrideventlooprpizero2w.sh
