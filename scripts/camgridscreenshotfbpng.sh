a70945293f04021a7e4350c2d69789cbdf286e34
Chris Paiano <chris.paiano@gmail.com>
2025-01-30 01:24:32 -0500

    Fixed cleanup of raw ss

--- scripts/camgridscreenshotfbpng.sh
+++ scripts/camgridscreenshotfbpng.sh
@@ -4,6 +4,6 @@ pushd ~
 cat /dev/fb0 > screenshot.raw
 # iraw2png 640 480 < screenshot.raw > screenshot.png
 ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 640x480 -i screenshot.raw -vframes 1 screenshot.png
-rm screen.raw
+rm screenshot.raw
 echo "Screenshot of framebuffer should now be saved as ~/screenshot.png"
 popd
