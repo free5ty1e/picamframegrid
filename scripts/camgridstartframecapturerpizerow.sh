cff9fe439146da535ba5ac2f4c76f1cd2753ccd5
Chris Paiano <chris.paiano@gmail.com>
2025-01-30 01:41:37 -0500

    trying another frameskip attempt

--- scripts/camgridstartframecapturerpizerow.sh
+++ scripts/camgridstartframecapturerpizerow.sh
@@ -33,10 +33,20 @@ while true; do
 		# 	-xoffset $XOFFSET -yoffset $YOFFSET -s $CAPTURE_RESOLUTION \
 		# 	/dev/fb0 </dev/null;
 
+		# nice -10 \
+		# 	ffmpeg -threads 1 -timeout $RTSP_TIMEOUT -err_detect ignore_err \
+		# 	-rtsp_transport udp -avioflags direct -fflags discardcorrupt+flush_packets -flags low_delay \
+		# 	-use_wallclock_as_timestamps 1 \
+		# 	-i "$STREAM_URL" \
+		# 	-vf 'setpts=PTS-STARTPTS' -pix_fmt rgb565le -preset ultrafast -an -y -f fbdev \
+		# 	-r $STREAM_FPS -fps_mode drop \
+		# 	-xoffset $XOFFSET -yoffset $YOFFSET -s $CAPTURE_RESOLUTION \
+		# 	/dev/fb0 </dev/null;
+
 		nice -10 \
 			ffmpeg -threads 1 -timeout $RTSP_TIMEOUT -err_detect ignore_err \
-			-rtsp_transport udp -avioflags direct -fflags discardcorrupt+flush_packets -flags low_delay \
-			-use_wallclock_as_timestamps 1 \
+			-rtsp_transport udp -avioflags direct -fflags nobuffer+discardcorrupt+flush_packets -flags low_delay \
+			-use_wallclock_as_timestamps 1 -vsync drop -max_delay 0 -rtbufsize 64K \
 			-i "$STREAM_URL" \
 			-vf 'setpts=PTS-STARTPTS' -pix_fmt rgb565le -preset ultrafast -an -y -f fbdev \
 			-r $STREAM_FPS -fps_mode drop \
