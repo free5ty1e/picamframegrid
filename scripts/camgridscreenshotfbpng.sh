70fb8b7c665e46330c16ceda9b82a466dd84e2a7
Chris Paiano <chris.paiano@gmail.com>
2025-01-30 01:22:37 -0500

    Now using ffmpeg to convert framebuffer dump to png

--- scripts/camgridscreenshotfbpng.sh
+++ scripts/camgridscreenshotfbpng.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 pushd ~
-cat /dev/fb0 > screen.raw
-iraw2png 640 480 < screen.raw > screen.png
+cat /dev/fb0 > screenshot.raw
+# iraw2png 640 480 < screenshot.raw > screenshot.png
+ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 640x480 -i screenshot.raw -vframes 1 screenshot.png
 rm screen.raw
 popd
