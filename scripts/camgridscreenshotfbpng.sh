1bce6dfc655fd00f04edbefb195d85cdf48bc058
Chris Paiano <chris.paiano@gmail.com>
2025-01-30 01:26:08 -0500

    Now storing prev screenshot and clearing before ffmpeg cmd

--- scripts/camgridscreenshotfbpng.sh
+++ scripts/camgridscreenshotfbpng.sh
@@ -3,6 +3,8 @@
 pushd ~
 cat /dev/fb0 > screenshot.raw
 # iraw2png 640 480 < screenshot.raw > screenshot.png
+rm screenshot.prev.png
+mv screenshot.png screenshot.prev.png
 ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 640x480 -i screenshot.raw -vframes 1 screenshot.png
 rm screenshot.raw
 echo "Screenshot of framebuffer should now be saved as ~/screenshot.png"
