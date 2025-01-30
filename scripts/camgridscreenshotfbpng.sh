a26dccc013b13d3d643905a5fbfe39cb9a537dc2
Chris Paiano <chris.paiano@gmail.com>
2025-01-30 01:23:25 -0500

    Echo after screenshot

--- scripts/camgridscreenshotfbpng.sh
+++ scripts/camgridscreenshotfbpng.sh
@@ -5,4 +5,5 @@ cat /dev/fb0 > screenshot.raw
 # iraw2png 640 480 < screenshot.raw > screenshot.png
 ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 640x480 -i screenshot.raw -vframes 1 screenshot.png
 rm screen.raw
+echo "Screenshot of framebuffer should now be saved as ~/screenshot.png"
 popd
